d82590680952939eed26b396b8c2de88c3ca0045
Andrew Learn <chc@thmods.com>
2017-05-06 01:39:29 -0400

    possibly fixed set player colour... need to find out why player info missing if so

--- scripts/OutboundConnectionManager.py
+++ scripts/OutboundConnectionManager.py
@@ -206,10 +206,10 @@ class OutboundConnectionManager():
 	def handleSetPlayerColour(self, connection, rpcid, rpc_data):
 		if rpc_data["id"] != self.playerid and rpc_data["id"] in self.player_info:
 			self.player_info[rpc_data["id"]]["colour"] = rpc_data["colour"]
-			if self.tool_settings["full_alpha"]:
-				rpc_data["colour"] = rpc_data["colour"] | 0x000000FF
-				self.client.connection.SendRPC(rpcid, rpc_data)
-				return False
+		if self.tool_settings["full_alpha"]:
+			rpc_data["colour"] = rpc_data["colour"] | 0x000000FF
+			self.client.connection.SendRPC(rpcid, rpc_data)
+			return False
 		return True
 	def handleAddPlayerToWorld(self, connection, rpcid, rpc_data):
 		self.player_info[rpc_data["id"]]["streamed"] = True
